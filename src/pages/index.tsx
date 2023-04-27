dc85c459fdb9cb7c715b1e11e272e62803595911
449sabu <449sabu@gmail.com>
2023-04-27 18:30:49 +0900

    💡 不要なJavaScriptコード削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,29 @@
-// import { KoiosProvider } from '@meshsdk/core';
 import { GetStaticProps, NextPage } from 'next';
 
 import Blog from '@/components/Blog';
 import Hero from '@/components/Hero';
 import Status from '@/components/Status';
-import { readConfig, reedSiteConfig } from '@/utils/config';
-import {
-  fetchDelegatorHistory,
-  fetchPoolInfo,
-  fetchPoolMetadata,
-} from '@/utils/pool_data';
+import { readConfig } from '@/utils/config';
+import { fetchPoolInfo } from '@/utils/pool_data';
 
 type Props = {
-  theme: string;
   poolInformation: PoolInformation;
   configuration: SiteConfig;
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  // const theme = reedSiteConfig();
-  const poolId = process.env.POOL_ID || '';
-  const poolInformation = await fetchPoolInfo(poolId);
-  const metadata = await fetchPoolMetadata(poolInformation.meta_url || '');
-  // const DelegatorHistory = await fetchDelegatorHistory(poolId);
-
+  const poolInformation = await fetchPoolInfo(process.env.POOL_ID || '');
   const configuration = readConfig();
-
-  const theme = process.env.NEXT_PUBLIC_THEME || '';
-  // console.log(theme)
-  // console.log(poolInformation);
 
   return {
     props: {
-      theme,
       poolInformation,
       configuration,
     },
   };
 };
 
-const Home: NextPage<Props> = ({ poolInformation, configuration, theme }) => {
+const Home: NextPage<Props> = ({ poolInformation }) => {
   const bgColor = 'bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400';
 
   return (
--- src/utils/pool_data.ts
+++ src/utils/pool_data.ts
@@ -18,35 +18,17 @@ export const fetchPoolInfo = async (
   return poolInformation;
 };
 
-export const fetchPoolMetadata = async (url: string) => {
-  const poolMetaDada = await fetch(`${url}`, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => console.log(error));
-  return poolMetaDada;
-};
-
-export const fetchDelegatorHistory = async (pool_id: string) => {
-  const delegatorHistory = fetch(
-    `https://api.koios.rest/api/v0/pool_delegators_history?_pool_bech32=${pool_id}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    },
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data;
-    })
-    .catch((error) => console.log(error));
-  return delegatorHistory;
-};
+// export const fetchPoolMetadata = async (url: string) => {
+//   const poolMetaDada = await fetch(`${url}`, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((res) => res.json())
+//     .then((data) => {
+//       return data;
+//     })
+//     .catch((error) => console.log(error));
+//   return poolMetaDada;
+// };
