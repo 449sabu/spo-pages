9e06157424e64c207243afaf08d6db175b4e7886
449sabu <449sabu@gmail.com>
2023-05-21 01:30:56 +0900

    format

--- src/utils/markdownToHtml/markdownToHtml.ts
+++ src/utils/markdownToHtml/markdownToHtml.ts
@@ -1,68 +1,68 @@
-import rehypeHighlight from "rehype-highlight";
-import rehypeMermaid from "rehype-mermaidjs";
-import remarkSlug from "rehype-slug";
-import rehypeStringify from "rehype-stringify";
-import remarkBreaks from "remark-breaks";
-import remarkGfm from "remark-gfm";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import { unified } from "unified";
+import rehypeHighlight from 'rehype-highlight';
+import rehypeMermaid from 'rehype-mermaidjs';
+import remarkSlug from 'rehype-slug';
+import rehypeStringify from 'rehype-stringify';
+import remarkBreaks from 'remark-breaks';
+import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 import {
-	remarkIndex,
-	rehypeIndex,
-	removeHljsClassName,
-	// print
-} from "@/utils/unified-plugin/unifiedPlugin";
+  detailsHandler,
+  remarkDetails,
+} from '@/utils/unified-plugin/detailsPlugin';
 import {
-	detailsHandler,
-	remarkDetails,
-} from "@/utils/unified-plugin/detailsPlugin";
+  remarkLinkCard,
+  linkCardHandler,
+} from '@/utils/unified-plugin/linkCardPlugin';
 import {
-	remarkLinkCard,
-	linkCardHandler,
-} from "@/utils/unified-plugin/linkCardPlugin";
+  remarkMessage,
+  messageHandler,
+  alertHandler,
+  remarkAlert,
+} from '@/utils/unified-plugin/messagePlugin';
 import {
-	remarkMessage,
-	messageHandler,
-	alertHandler,
-	remarkAlert,
-} from "@/utils/unified-plugin/messagePlugin";
+  remarkIndex,
+  rehypeIndex,
+  removeHljsClassName,
+  // print
+} from '@/utils/unified-plugin/unifiedPlugin';
 
 export const markdownToHtml = async (markdownContent: string) => {
-	const result = await unified()
-		.use(remarkParse)
-		.use(remarkBreaks)
-		// .use(print)
-		.use([remarkDetails, remarkAlert, remarkMessage, remarkLinkCard])
-		.use(remarkGfm)
-		.use(remarkRehype, {
-			handlers: {
-				linkcard: linkCardHandler,
-				details: detailsHandler,
-				message: messageHandler,
-				alert: alertHandler,
-			},
-		})
-		.use(remarkSlug)
-		.use(rehypeMermaid)
-		.use(rehypeHighlight, {
-			detect: true,
-		})
-		.use(removeHljsClassName)
-		.use(rehypeStringify)
-		.process(markdownContent);
-	return result.toString();
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkBreaks)
+    // .use(print)
+    .use([remarkDetails, remarkAlert, remarkMessage, remarkLinkCard])
+    .use(remarkGfm)
+    .use(remarkRehype, {
+      handlers: {
+        linkcard: linkCardHandler,
+        details: detailsHandler,
+        message: messageHandler,
+        alert: alertHandler,
+      },
+    })
+    .use(remarkSlug)
+    .use(rehypeMermaid)
+    .use(rehypeHighlight, {
+      detect: true,
+    })
+    .use(removeHljsClassName)
+    .use(rehypeStringify)
+    .process(markdownContent);
+  return result.toString();
 };
 
 export const markdownToIndex = async (markdown: string): Promise<string> => {
-	const result = await unified()
-		.use(remarkParse)
-		.use(remarkIndex)
-		.use(remarkRehype)
-		.use(remarkSlug)
-		.use(rehypeIndex)
-		.use(rehypeStringify)
-		.process(markdown);
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkIndex)
+    .use(remarkRehype)
+    .use(remarkSlug)
+    .use(rehypeIndex)
+    .use(rehypeStringify)
+    .process(markdown);
 
-	return result.toString();
+  return result.toString();
 };
--- src/utils/mdx-remote/serializer.ts
+++ src/utils/mdx-remote/serializer.ts
@@ -1,72 +1,72 @@
-import { serialize } from "next-mdx-remote/serialize";
-import rehypeHighlight from "rehype-highlight";
-import rehypeMermaid from "rehype-mermaidjs";
-import rehypeSlug from "rehype-slug";
-import remarkBreaks from "remark-breaks";
-import remarkGfm from "remark-gfm";
+import { serialize } from 'next-mdx-remote/serialize';
+import rehypeHighlight from 'rehype-highlight';
+import rehypeMermaid from 'rehype-mermaidjs';
+import rehypeSlug from 'rehype-slug';
+import remarkBreaks from 'remark-breaks';
+import remarkGfm from 'remark-gfm';
 import {
-	messageHandler,
-	remarkMessage,
-	remarkAlert,
-	alertHandler,
-} from "../unified-plugin/messagePlugin";
+  messageHandler,
+  remarkMessage,
+  remarkAlert,
+  alertHandler,
+} from '../unified-plugin/messagePlugin';
 import {
-	removeHljsClassName,
-	print,
-} from "@/utils/unified-plugin/unifiedPlugin";
+  detailsHandler,
+  remarkDetails,
+} from '@/utils/unified-plugin/detailsPlugin';
 import {
-	detailsHandler,
-	remarkDetails,
-} from "@/utils/unified-plugin/detailsPlugin";
+  remarkLinkCard,
+  linkCardHandler,
+} from '@/utils/unified-plugin/linkCardPlugin';
 import {
-	remarkLinkCard,
-	linkCardHandler,
-} from "@/utils/unified-plugin/linkCardPlugin";
+  removeHljsClassName,
+  print,
+} from '@/utils/unified-plugin/unifiedPlugin';
 
 export const serializer = async (source: string) => {
-	const result = await serialize(source, {
-		scope: {},
-		mdxOptions: {
-			remarkPlugins: [
-				print,
-				remarkBreaks,
-				remarkDetails,
-				remarkAlert,
-				remarkMessage,
-				remarkLinkCard,
-				remarkGfm,
-			],
-			rehypePlugins: [
-				rehypeSlug,
-				[
-					rehypeMermaid,
-					{
-						strategy: "inline-svg",
-						mermaidConfig: {
-							fontFamily: "sans-serif, monospace",
-						},
-					},
-				],
-				[
-					rehypeHighlight,
-					{
-						detect: true,
-					},
-				],
-				removeHljsClassName,
-			],
-			remarkRehypeOptions: {
-				handlers: {
-					linkCard: linkCardHandler,
-					message: messageHandler,
-					alert: alertHandler,
-					details: detailsHandler,
-				},
-			},
-			format: "mdx",
-		},
-		parseFrontmatter: true,
-	});
+  const result = await serialize(source, {
+    scope: {},
+    mdxOptions: {
+      remarkPlugins: [
+        print,
+        remarkBreaks,
+        remarkDetails,
+        remarkAlert,
+        remarkMessage,
+        remarkLinkCard,
+        remarkGfm,
+      ],
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeMermaid,
+          {
+            strategy: 'inline-svg',
+            mermaidConfig: {
+              fontFamily: 'sans-serif, monospace',
+            },
+          },
+        ],
+        [
+          rehypeHighlight,
+          {
+            detect: true,
+          },
+        ],
+        removeHljsClassName,
+      ],
+      remarkRehypeOptions: {
+        handlers: {
+          linkCard: linkCardHandler,
+          message: messageHandler,
+          alert: alertHandler,
+          details: detailsHandler,
+        },
+      },
+      format: 'mdx',
+    },
+    parseFrontmatter: true,
+  });
 
-	return result;
+  return result;
 };
